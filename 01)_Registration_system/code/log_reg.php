549dc24bb8818615f42c8c855ff91dc4bfbb2f21
shilpa511raghav <shilpa511raghav@gmail.com>
2018-07-06 07:10:47 +0530

    registration system using php and mysql

--- 01)_Registration_system/code/log_reg.php
+++ 01)_Registration_system/code/log_reg.php
@@ -2,31 +2,55 @@
 $host= "localhost";
 $user= "root";
 $pass="";
-$dbname= "registration";
+$dbname= "testdb";
 
-$connection= mysqli_connect($host,$user,$pass,$dbname);
-
+$conn= mysqli_connect($host,$user,$pass,$dbname);
 if (isset($_POST['save'])){
-$nam= $_POST['name'];
-$uname= $_POST[''];
-$mail= $_POST['email'];
-$pwd= $_POST['password'];
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	$nam= $_POST["name"];
+	$uname= $_POST["username"];
+	$mail= $_POST["email"];
+	$pwd= $_POST["password"];
+	if($nam=='')  
+		{  
+			echo"<script>alert('Please enter the name')</script>";  
+	exit();//this use if first is not work then other will not show  
+		} 
+		
+	if($uname=='')  
+		{  
+			echo"<script>alert('Please enter the username')</script>";  
+	exit();	
+		}
+	if($pwd=='')  
+		{  
+			echo"<script>alert('Please enter the password')</script>";  
+	exit();  
+		}  
+	  
+	if($mail=='')  
+		{  
+			echo"<script>alert('Please enter the email')</script>";  
+	exit(); 	
+		}
+	
+	$check_email_query="select * from user_info WHERE email='$mail'"; 
+    $run_query=mysqli_query($conn,$check_email_query);
+	
+	if(mysqli_num_rows($run_query)>0)  
+    {  
+	echo "<script>alert('Email $mail is already exist in our database, Please try another one!')</script>";  
+	exit();  
+		}  
+	
+$insert_user= "insert into user_info(name,username,email,password) VALUES ('$nam','$uname','$mail','$pwd')";	
+$result= mysqli_query($conn  ,$insert_user);
+if ($result==true){
+	echo"user registered";
+}
+else{
+	echo "registration failed";
+}
+}
 ?>
 
 
@@ -47,7 +71,7 @@ $pwd= $_POST['password'];
 
 
 <body>
-	<div class= "container">
+	<div class= "container cont">
 		<div class= "row main">
 			<div class= "panel-heading">
 				<div class= "panel-title text-center">
@@ -57,8 +81,8 @@ $pwd= $_POST['password'];
 			</div>
 			
 			<div>
-				<div class = "main-login main-center">
-					<form class= "form-horizontal" action = "#" method= "post">
+				<div class= "main-login main-center">
+					<form class= "form-horizontal" action = "" method= "post">
 						<div class= "form-group">
 						<label class= "cols-sm-2 control-label">Name</label>
 							<div class= "col-sm-10">     
@@ -75,8 +99,8 @@ $pwd= $_POST['password'];
 						<label class= "cols-sm-2 control-label">Username</label>
 							<div class= "col-sm-10">     
 								<div class="input-group">
-										<span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
-										<input type="text" class="form-control" name="username" id="name"  placeholder="Enter your username"/>
+										<span class="input-group-addon"><i class="fa fa-users fa" aria-hidden="true"></i></span>
+										<input type="text" class="form-control" name="username" id="username"  placeholder="Enter your username"/>
 								</div>
 							
 							</div>
@@ -86,8 +110,8 @@ $pwd= $_POST['password'];
 						<label class= "cols-sm-2 control-label">Email</label>
 							<div class= "col-sm-10">     
 								<div class="input-group">
-										<span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
-										<input type="text" class="form-control" name="username" id="name"  placeholder="Enter your email"/>
+										<span class="input-group-addon"><i class="fa fa-envelope fa" aria-hidden="true"></i></span>
+										<input type="text" class="form-control" name="email" id="email"  placeholder="Enter your email"/>
 								</div>
 							
 							</div>
@@ -97,25 +121,16 @@ $pwd= $_POST['password'];
 						<label class= "cols-sm-2 control-label">Password</label>
 							<div class= "col-sm-10">     
 								<div class="input-group">
-										<span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
-										<input type="text" class="form-control" name="password" id="name"  placeholder="Enter password"/>
+										<span class="input-group-addon"><i class="fa fa-lock lg-fa" aria-hidden="true"></i></span>
+										<input type="password" class="form-control" name="password" id="name"  placeholder="Enter password"/>
 								</div>
-							
+							</div>
 						</div>
 						
-						</div>
-						<div class="form-group">
-								<label for="confirm" class="cols-sm-2 control-label">Confirm Password</label>
-								<div class="cols-sm-10">
-									<div class="input-group">
-										<span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
-										<input type="password" class="form-control" name="confirm" id="confirm"  placeholder="Confirm your Password"/>
-									</div>
-								</div>
-						</div>
+						
 					
 						<div class="form-group ">
-								<button type="button" name= "save" class="btn btn-primary btn-lg btn-block login-button">Register</button>
+								<button type="submit" name= "save" class="btn btn-primary btn-lg btn-block login-button">Register</button>
 						</div>
 						<div class="login-register">
 								<a href="index.php">Login</a>
@@ -127,8 +142,8 @@ $pwd= $_POST['password'];
 				</div>
 			
 			</div>
-		
 		</div>
+		
 
 	</div>
 	<script type="text/javascript" src="assets/js/bootstrap.js"></script>
