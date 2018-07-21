fa6b5b94cea086f532ca32f65a0fe5d33b129e01
shilpa511raghav <shilpa511raghav@gmail.com>
2018-07-21 08:58:01 +0530

    Online Crime reporting system

---
+++ 02)_Crime_Reporting_System/index.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<title>Crime Reporting System</title>
+</head>
+<body>
+<nav class= "navbar navbar-fixed-top navbar-inverse">
+	<div class= "container">
+		<div class= "navbar header">
+			<a class = "navbar-brand" href = "index.php" style= "color:white">Report a Crime!</a>
+		</div>	
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1" >
+				<ul class="nav navbar-nav navbar-right">
+					<li>
+						<a href="#" style="color: white">About</a>
+					</li>
+					<li>
+						<a href="#" style="color: white">Services</a>
+					</li>
+					<li>
+						<a href="#" style="color: white">Contact</a>
+					</li>
+	  
+					<li>
+						<a href="#" style="color: white">Blog</a>
+					</li>
+					<li>
+						<a href="#" style="color: white">FAQ</a>
+						
+					</li>
+					
+					<a href="#"class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal" style="padding: 5px;border-radius: 2px;box-sizing: content-box;margin-top: 7px" >
+					   <?php echo "Login / Register"; ?></a>
+					   
+					
+					
+					
+				</ul>	
+			
+			</div>
+	</div>
+
+</nav>
+</body>
+
+</html>
